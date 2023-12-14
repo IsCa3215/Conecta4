adb0f335af0e0643b11a7653c63a117681752dcf
IsCa3215 <81421517+IsCa3215@users.noreply.github.com>
2023-12-14 19:28:02 +0100

    a

--- src/Funciones.java
+++ src/Funciones.java
@@ -1,5 +1,6 @@
 
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Funciones {
@@ -8,7 +9,6 @@ public class Funciones {
     public static final int FICHA_CRUZ = 2;
     public static boolean PARTIDA_FIN = false;
 
-    public static int num_linea = 0;
 
     public static int leerInt(String mensaje) {
         Scanner leer = new Scanner(System.in);
@@ -18,7 +18,8 @@ public class Funciones {
             System.out.println("\u001B[31m" + "No se ha introducido un valor válido, Inténtalo de nuevo" + "\u001B[0m");
 
         }
-        int valor2 = leer.nextInt();
+        int valor2;
+        valor2 = leer.nextInt();
         return valor2;
     }
 
@@ -39,7 +40,7 @@ public class Funciones {
     public static void mostrarTablero(int[][] tablero) {//Esta función muestra el tablero y modifica los valores de las casillas por otros
 
         String casilla = null;
-        for (int l = 0; l < tablero[0].length-1; l++) {
+        for (int l = 0; l < tablero.length; l++) {
             System.out.print("   " + l + " ");
         }
         System.out.println();
@@ -69,7 +70,7 @@ public class Funciones {
 
     }
 
-    public static void mostrarTablero(int[][] tablero, int columna){
+    public static void mostrarTablero(int[][] tablero, int columna) {
         final String RESET = "\u001B[0m";
         final String RED_BOLD = "\033[1;31m";
 
@@ -139,24 +140,25 @@ public class Funciones {
         int contador_horizontal = 0;
 
 
-        for (int i = 0; i < tablero[0].length; i++) {
+        for (int i = 0; i < tablero[0].length; i++) {//añade 1 al contador cuando detecta que en una parte hay una ficha
             if (tablero[ultima_Fila][i] == ficha) {
                 contador_horizontal++;
-                if (contador_horizontal >= n) {
-                    return true;
+                if (contador_horizontal >= n) {// cuando el contador llegue a 4 devuelve true.
+                    return PARTIDA_FIN = true;
+
                 }
             } else {
                 contador_horizontal = 0;
             }
         }
 
-
+// contador vertical
         int contador_vertical = 0;
         for (int i = 0; i < tablero.length; i++) {
             if (tablero[i][columna] == ficha) {
                 contador_vertical++;
                 if (contador_vertical >= n) {
-                    return true;
+                    return PARTIDA_FIN = true;
                 }
             } else {
                 contador_vertical = 0;
@@ -169,7 +171,7 @@ public class Funciones {
                     if (fila + k < tablero.length && columna_c + k < tablero[0].length && tablero[fila + k][columna_c + k] == ficha) {
                         contador++;
                         if (contador >= n) {
-                            return true;
+                            return PARTIDA_FIN = true;
                         }
                     } else {
                         break;
@@ -177,9 +179,82 @@ public class Funciones {
                 }
             }
         }
+        for (int fila = 0; fila < tablero.length; fila++) {
+            for (int columna_c = tablero[0].length - 1; columna_c >= 0; columna_c--) {
+                int contador = 0;
+                for (int k = 0; k < n; k++) {
+                    if (fila + k < tablero.length && columna_c - k >= 0 && tablero[fila + k][columna_c - k] == ficha) {
+                        contador++;
+                        if (contador >= n) {
+                            return PARTIDA_FIN = true;
+                        }
+                    } else {
+                        break;
+                    }
 
-
-
+                }
+            }
+        }
         return false;
     }
+
+    public static boolean turnoJugador(int tablero[][], String nombre, int ficha, int modo, int turno, int nivel_bot) {
+
+
+        if (turno == 1 && modo == 1 || modo == 2) {
+            int columna = Funciones.leerIntValue("Dónde deseas colocar la ficha " + nombre + "?", 0, tablero.length);
+
+            boolean colocar = Funciones.colocarFicha(tablero, ficha, columna);
+            boolean comprobar_j1 = Funciones.comprobarLinea(tablero, columna, 4);
+            Funciones.mostrarTablero(tablero, columna);
+            if (comprobar_j1) {
+                System.out.println("---------------------------------------------------");
+                Funciones.mostrarTablero(tablero, columna);
+                System.out.println(nombre + "\u001B[32m" + " Ha ganado la partida!" + "\u001B[0m");
+                return comprobar_j1;
+            }
+            while (!colocar) {
+                System.out.println("\u001B[31m" + "No se ha podido colocar la ficha!" + "\u001B[0m");
+                columna = Funciones.leerInt("Dónde deseas colocar la ficha " + nombre + "?");
+                colocar = Funciones.colocarFicha(tablero, ficha, columna);
+                Funciones.comprobarLinea(tablero, columna, 4);
+                Funciones.mostrarTablero(tablero, columna);
+            }
+
+        } else if (modo == 1) {
+            Random bot = new Random();
+            boolean colocar = false;
+            int columna_bot;
+            do {
+                columna_bot = bot.nextInt(tablero.length);
+                colocar = Funciones.colocarFicha(tablero, FICHA_CRUZ, columna_bot);
+            } while (!colocar);
+            boolean comprobar_bot = Funciones.comprobarLinea(tablero, columna_bot, 4);
+            if (comprobar_bot) {
+                System.out.println("---------------------------------------------------");
+                Funciones.mostrarTablero(tablero, columna_bot);
+                System.out.println("El bot" + "\u001B[32m" + " Ha ganado la partida!" + "\u001B[0m");
+                return comprobar_bot;
+            }
+            Funciones.mostrarTablero(tablero, columna_bot);
+        }
+        return false;
+    }
+
+    public static boolean casillasLibres(int[][] tablero) {
+        int contador = 0;
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                if (tablero[i][j] != CASILLA_VACIA) {
+                    contador++;
+                }
+            }
+        }
+        if (contador < tablero.length) {
+            return true;
+        }
+        return false;
+    }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -21,134 +21,83 @@ public class Main {
         int[][] tablero = new int[filas_texto][columnas_texto];//Para int tablero vamos a asignarle los valores leidos por teclado.
         Funciones.mostrarTablero(tablero);
         int modo;
-        do {
-            modo = Funciones.leerInt("Seleccione el modo de juego:" + "\n" + "1. Un jugador" + "\n" + "2. Dos Jugadores");//Leemos que ha introducido el usuario(1(un jugador) o 2(dos jugadores)).
-        } while (modo > 2 || modo < 1);
+
+        modo = Funciones.leerIntValue("Seleccione el modo de juego:" + "\n" + "1. Un jugador" + "\n" + "2. Dos Jugadores", 1, 2);//Leemos que ha introducido el usuario(1(un jugador) o 2(dos jugadores)).
 
 
-        if (modo == 1) {//Si el modo de juego es de un jugador se activa la condición de un jugador
-            Random bot = new Random();
+        //Si el modo de juego es de un jugador se activa la condición de un jugador
+        Random bot = new Random();
+        String nombre_j1 = " ";
+        String nombre_j2 = " ";
+        if (modo == 1) {
             System.out.println("Introduce tu nombre jugador!");
-            String nombre_j1 = leer.next();
-            int nivel_bot = 0;
+            nombre_j1 = leer.next();
+        }
+        if (modo == 2) {
+            System.out.println("Introduce tu nombre jugador 1");
+            nombre_j1 = leer.next();
+            System.out.println("Introduce tu nombre jugador 2");
+            nombre_j2 = leer.next();
+        }
 
+        int nivel_bot = 0;
 
+        if (modo == 1) {
             nivel_bot = Funciones.leerIntValue("Introduce un nivel", 1, 2);
+        }
 
 
-            if (nivel_bot == 1) {
-                int turno = 1;
-                while (!PARTIDA_FIN) {
-                    boolean colocar_j1 = false;
-                    System.out.println("Prepárate " + nombre_j1);
-                    int columna_j1 = 0;
-                    int columna_bot = 0;
-
-                    if (turno == 1) {
-                        columna_j1 = Funciones.leerInt("Dónde deseas colocar la ficha " + nombre_j1 + "?");
-
-                        while (columna_j1 < 0 || columna_j1 > tablero.length) {
-                            System.out.println("\u001B[31m" + "Introduce un valor válido" + "\u001B[0m");
-                            columna_j1 = Funciones.leerInt("Dónde deseas colocar la ficha " + nombre_j1 + "?");
-
-                        }
-                        colocar_j1 = Funciones.colocarFicha(tablero, FICHA_CIRCULO, columna_j1);
-                        boolean comprobar_j1 = Funciones.comprobarLinea(tablero, columna_j1, 4);
-                        Funciones.mostrarTablero(tablero, columna_j1);
-                        if (comprobar_j1) {
-                            System.out.println("---------------------------------------------------");
-                            Funciones.mostrarTablero(tablero, columna_j1);
-                            System.out.println(nombre_j1 + "\u001B[32m" + " Ha ganado la partida!" + "\u001B[0m");
-
-                            return;
-                        }
-                        while (!colocar_j1) {
-                            System.out.println("\u001B[31m" + "No se ha podido colocar la ficha!" + "\u001B[0m");
-                            columna_j1 = Funciones.leerInt("Dónde deseas colocar la ficha " + nombre_j1 + "?");
-                            colocar_j1 = Funciones.colocarFicha(tablero, FICHA_CIRCULO, columna_j1);
-                            Funciones.comprobarLinea(tablero, columna_j1, 4);
-                            Funciones.mostrarTablero(tablero, FICHA_CIRCULO);
-                        }
-
-                        turno--;
+        while (!PARTIDA_FIN) {
+            if (modo == 1) {
+                System.out.println("Prepárate " + nombre_j1);
+                int columna_j1 = 0;
+                int columna_bot = 0;
+                int contador = 1;
+                if (contador == 1) {
+                    boolean casillas_libres = Funciones.casillasLibres(tablero);
+                    if (!casillas_libres){
+                        System.out.println("Empate!");
+                        return;
 
                     }
-                    if (turno == 0) {
-                        columna_bot = bot.nextInt(tablero.length);
-                        Funciones.colocarFicha(tablero, FICHA_CRUZ, columna_bot);
-                        boolean comprobar_bot = Funciones.comprobarLinea(tablero, columna_bot, 4);
-                        System.out.println("--------------bot-------------");
-
-                        if (comprobar_bot) {
-                            Funciones.mostrarTablero(tablero, FICHA_CIRCULO);
-                            System.out.println("El bot" + "\u001B[32m" + " Ha ganado la partida!" + "\u001B[0m");
-
-                            return;
-                        }
-                        Funciones.mostrarTablero(tablero);
-                        turno++;
-
+                    boolean ver = Funciones.turnoJugador(tablero, nombre_j1, FICHA_CIRCULO, modo, 1, nivel_bot);
+                    contador--;
+                    if (ver) {
+                        return;
+                    }
+                }
+                if (contador == 0) {
+                    boolean casillas_libres = Funciones.casillasLibres(tablero);
+                    if (!casillas_libres){
+                        System.out.println("Empate!");
+                        return;
 
                     }
+                    boolean ver = Funciones.turnoJugador(tablero, "bot", FICHA_CRUZ, modo, 0, nivel_bot);
+                    if (ver) {
+                        return;
+                    }
+                    contador++;
+                }
 
+
+            } else if (modo == 2) {
+                int contador = 1;
+                if (contador == 1) {
+                    boolean ver = Funciones.turnoJugador(tablero, nombre_j1, FICHA_CIRCULO, modo, 1, 1);
+                    if (ver) {
+                        return;
+                    }
+                    contador++;
+                }
+                if (contador == 2) {
+                    boolean ver = Funciones.turnoJugador(tablero, nombre_j2, FICHA_CRUZ, modo, 0, 1);
+                    if (ver) {
+                        return;
+                    }
+                    contador--;
                 }
             }
-            if (nivel_bot == 2){
-
-
-            }
-
-        } else if (modo == 2) {//Si el modo de juego es de dos jugadores se activa la condición dos jugadores
-            int turno = 1;
-            int columna_jug1;
-            int columna_jug2;
-            boolean colocar = false;
-            System.out.println("Introduce tu nombre jugador 1!");
-            String nombre_j1 = leer.next();
-            System.out.println("Introduce tu nombre jugador 2!");
-            String nombre_j2 = leer.next();
-            System.out.println("Estáis listos " + nombre_j2 + " y " + nombre_j1 + "? Empecemos!");
-            while (!PARTIDA_FIN) {
-
-
-                if (turno == 1) {
-                    columna_jug1 = Funciones.leerInt("Dónde irá tu ficha " + nombre_j1 + "?");
-                    while (columna_jug1 < 0 || columna_jug1 > tablero.length) {
-                        System.out.println("\u001B[31m" + "No has introducido un valor válido" + "\u001B[0m");
-                        columna_jug1 = Funciones.leerInt("Dónde deseas colocar la ficha " + nombre_j1 + "?");
-                        Funciones.mostrarTablero(tablero);
-                    }
-                    colocar = Funciones.colocarFicha(tablero, FICHA_CIRCULO, columna_jug1);
-                    Funciones.mostrarTablero(tablero);
-                    while (!colocar) {
-                        System.out.println("\u001B[31m" + "No se ha podido colocar la ficha!" + "\u001B[0m");
-                        columna_jug1 = Funciones.leerInt("Dónde deseas colocar la ficha " + nombre_j1 + "?");
-                        colocar = Funciones.colocarFicha(tablero, FICHA_CIRCULO, columna_jug1);
-                        Funciones.mostrarTablero(tablero);
-                    }
-
-                    turno--;
-                }
-                if (turno == 0) {
-                    columna_jug2 = Funciones.leerInt("Es tu turno " + nombre_j2);
-                    while (columna_jug2 < 0 || columna_jug2 > tablero.length) {
-                        System.out.println("\u001B[31m" + "No has introducido un valor válido" + "\u001B[0m");
-                        columna_jug2 = Funciones.leerInt("Dónde deseas colocar la ficha " + nombre_j2 + "?");
-                        Funciones.mostrarTablero(tablero);
-                    }
-                    colocar = Funciones.colocarFicha(tablero, FICHA_CRUZ, columna_jug2);
-                    Funciones.mostrarTablero(tablero, FICHA_CIRCULO);
-                    while (!colocar) {
-                        System.out.println("\u001B[31m" + "No se ha podido colocar la ficha!" + "\u001B[0m");
-                        columna_jug2 = Funciones.leerInt("Dónde deseas colocar la ficha " + nombre_j2 + "?");
-                        colocar = Funciones.colocarFicha(tablero, FICHA_CRUZ, columna_jug2);
-                        Funciones.mostrarTablero(tablero, FICHA_CIRCULO);
-                    }
-                    turno++;
-                }
-            }
-
-
         }
     }
 }
